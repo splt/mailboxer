7c243e78e1846f9fc906e28b3ce30af0906ddf37
Cameron Barker <barker.cameron@gmail.com>
2014-04-24 13:38:45 -0400

    making is_read/unread scope look for a string

--- app/models/mailboxer/receipt.rb
+++ app/models/mailboxer/receipt.rb
@@ -27,8 +27,8 @@ class Mailboxer::Receipt < ActiveRecord::Base
   scope :not_trash, lambda { where(:trashed => false) }
   scope :deleted, lambda { where(:deleted => true) }
   scope :not_deleted, lambda { where(:deleted => false) }
-  scope :is_read, lambda { where(:is_read => true) }
-  scope :is_unread, lambda { where(:is_read => false) }
+  scope :is_read, lambda { where(:is_read => "true") }
+  scope :is_unread, lambda { where(:is_read => "false") }
 
   after_validation :remove_duplicate_errors
   class << self
